bd743358e39f9cb5d0bcf56ae8380829cb3861b6
Asifshaik_ <53392484+asifshaik02@users.noreply.github.com>
2021-05-02 22:11:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //load html with contents in "details.json"
-fetch("/data/details.json")
+fetch("web-dev-projects/data/details.json")
     .then(response => response.json())
     .then(data =>{
         l = [];
